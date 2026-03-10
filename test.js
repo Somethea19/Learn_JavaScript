088589f7e20575d95b72c1f2161cf897e53ca8a0
Somethea19 <sometheaoeung45@gmail.com>
2026-03-10 09:32:50 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+// let input = prompt("Hi how are you");
+// console.log(input);
+// console.log("If you want to see it look in console");
+// console.log(Math.floor(Math.random() * 100));
+const NAME = prompt("Write Your Name");
+console.log(NAME);
