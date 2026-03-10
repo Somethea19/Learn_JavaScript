04b37537dbc19f44a59e8c288df541f084014ec2
Somethea19 <sometheaoeung45@gmail.com>
2026-03-10 09:58:16 +0700

    number

---
+++ number.js
@@ -0,0 +1,6 @@
+let intNr = 1;
+let decNr = 1.5;
+let expNr = 1.4e15;
+let octNr = 0o10; //decimal version would be 8
+let hexNr = 0x3E8; //decimal version would be 1000
+let binNr = 0b101; //decimal version would be 5
